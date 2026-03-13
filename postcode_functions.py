bcadf97b7279088b27c158edba3c83b8987daf99
Aarij <aarijb@outlook.com>
2026-03-13 15:28:24 +0000

    passing all tests

--- postcode_functions.py
+++ postcode_functions.py
@@ -27,12 +27,13 @@ def validate_postcode(postcode: str) -> bool:
     response = req.get(
         f"https://api.postcodes.io/postcodes/{postcode}/validate")
 
+    if response.status_code == 500:
+        raise req.RequestException("Unable to access API.")
+
     data = response.json()
 
     if response.status_code == 200:
         return data["result"]
-    elif response.status_code == 500:
-        raise req.RequestException("Unable to access API.")
     else:
         raise TypeError("Function expects a string.")
 
@@ -46,14 +47,15 @@ def get_postcode_for_location(lat: float, long: float) -> str:
         f"https://api.postcodes.io/postcodes?lon={long}&lat={lat}"
     )
 
+    if response.status_code == 500:
+        raise req.RequestException("Unable to access API.")
+
     data = response.json()
 
     if response.status_code == 200:
         if data["result"] is None:
             raise ValueError("No relevant postcode found.")
         return data["result"][0]["postcode"]
-    elif response.status_code == 500:
-        raise req.RequestException("Unable to access API.")
     else:
         raise ValueError("No relevant postcode found.")
 
@@ -64,17 +66,36 @@ def get_postcode_completions(postcode_start: str) -> list[str]:
 
     response = req.get(
         f"https://api.postcodes.io/postcodes/{postcode_start}/autocomplete")
-    data = response.json()
 
+    if response.status_code == 500:
+        raise req.RequestException("Unable to access API.")
+
+    data = response.json()
     if response.status_code == 200:
         if data["result"] is None:
             raise ValueError("No postcode completions found.")
         return data["result"]
-    elif response.status_code == 500:
-        raise req.RequestException("Unable to access API.")
     else:
         raise ValueError("No postcode completions found.")
 
 
 def get_postcodes_details(postcodes: list[str]) -> dict:
-    pass
+    if not isinstance(postcodes, list):
+        raise TypeError("Function expects a list of strings.")
+
+    for p in postcodes:
+        if not isinstance(p, str):
+            raise TypeError("Function expects a list of strings.")
+
+    response = req.post("https://api.postcodes.io/postcodes",
+                        json={"postcodes": postcodes})
+
+    if response.status_code == 500:
+        raise req.RequestException("Unable to access API.")
+
+    data = response.json()
+
+    if response.status_code == 200:
+        return data["result"]
+    else:
+        raise ValueError("No postcode completions found.")
