3a7cca8d04616b74c18c3b87a74a34c2b89342e5
Aarij <aarijb@outlook.com>
2026-03-13 15:43:12 +0000

    adding first argument

--- postcode_cli.py
+++ postcode_cli.py
@@ -1,6 +1,23 @@
 """A CLI application for interacting with the Postcode API."""
 
 from argparse import ArgumentParser
+from postcode_functions import validate_postcode, get_postcode_completions
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", '-m', required=True,
+                        choices=["validate", "complete"])
+    parser.add_argument("postcode", type=str)
+    args = parser.parse_args()
+
+    postcode = args.postcode.strip().upper()
+
+    if args.mode == "validate":
+        if validate_postcode(postcode):
+            print(f"{postcode} is a valid postcode")
+        else:
+            print(f"{postcode} is not valid")
 
 
 if __name__ == "__main__":
